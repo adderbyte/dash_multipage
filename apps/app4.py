ca037a1e13d00319f9aa33c8dd7a298054501282
Kumar Prakhar <kumarprkhr@gmail.com>
2021-08-11 09:28:13 +0530

    Update app4.py

--- apps/app4.py
+++ apps/app4.py
@@ -12,7 +12,6 @@ import requests
 import pandas as pd
 import io
 from PIL import Image
-
 from app import app, cache
 
 final_nlp = pd.read_csv("./assets/finaldf.csv")
@@ -60,7 +59,7 @@ def word_cloud_2():
     fig = px.imshow(wc)
     return fig
 
-Layout = dbc.Container(html.Div([html.H1("NLP", style={
+BODY = dbc.Container(html.Div([html.H1("NLP", style={
     'textAlign': 'center',
     "font-family": "'Fredoka One', cursive",
     "font-size": "48px",
@@ -150,6 +149,39 @@ Layout = dbc.Container(html.Div([html.H1("NLP", style={
                 "-webkit-box-shadow": "0px 0px 8px 8px #ebe9e8","margin-bottom": "30px", "margin-top": "30px","padding":"1%"}),
 
                 html.Br(), html.Br(),
+                dbc.CardBody(html.Div([ dbc.CardHeader(html.H4("Word Response - Turkish", style={'text-align': 'center', 'font-weight': 800}), style={
+                   "border-radius": "20px 20px 0px 0px"}),
+                dcc.Dropdown(
+                    id='words2-1',
+                    options=[{'label': i, 'value': i} for i in turkish],
+                    value='positive',
+                    placeholder='Filter by sentiment...'
+                ),
+                dcc.Dropdown(
+                    id='word2-id',
+                    options=[{'label': i, 'value': i}
+                            for i in Turkishnlp.trProcessed.unique()],
+                    value='twitter üzerinden reklamlar devam ederse başarılı olacağını düşünüyorum',
+                    placeholder='Filter by words ...'
+                ),
+                # html.P("Country:"),
+                # dcc.Dropdown(
+                #     id='country-id', 
+                #     value='Turkey', 
+                #     options=[{'value': x, 'label': x} 
+                #             for x in country],
+                #     clearable=False
+                # ),
+                dcc.Graph(id ='table-2', config={
+        "displaylogo": False
+    },
+                    ),
+                
+            ]),
+                style={"border-radius": "20px", "box-shadow": "0px 0px 8px 8px #ebe9e8",
+                "-webkit-box-shadow": "0px 0px 8px 8px #ebe9e8","margin-bottom": "30px", "margin-top": "30px","padding":"1%"}),
+
+                html.Br(), html.Br(),
                 dbc.CardBody(html.Div([ dbc.CardHeader(html.H4("Word Cloud - English", style={'text-align': 'center', 'font-weight': 800}), style={
                    "border-radius": "20px 20px 0px 0px"}),
                 dcc.Dropdown(
@@ -174,6 +206,39 @@ Layout = dbc.Container(html.Div([html.H1("NLP", style={
                 #     clearable=False
                 # ),
                 dcc.Graph(id ='word-1', config={
+        "displaylogo": False
+    },
+                    ),
+                
+            ]),
+                style={"border-radius": "20px", "box-shadow": "0px 0px 8px 8px #ebe9e8",
+                "-webkit-box-shadow": "0px 0px 8px 8px #ebe9e8","margin-bottom": "30px", "margin-top": "30px","padding":"1%"}),
+
+                html.Br(), html.Br(),
+                dbc.CardBody(html.Div([ dbc.CardHeader(html.H4("Word Response - English", style={'text-align': 'center', 'font-weight': 800}), style={
+                   "border-radius": "20px 20px 0px 0px"}),
+                dcc.Dropdown(
+                    id='words-1',
+                    options=[{'label': i, 'value': i} for i in english],
+                    value='POSITIVE',
+                    placeholder='Filter by sentiment...'
+                ),
+                dcc.Dropdown(
+                    id='word-id',
+                    options=[{'label': i, 'value': i}
+                            for i in Englishnlp.enProcessed.unique()],
+                    value='super',
+                    placeholder='Filter by words ...'
+                ),
+                # html.P("Country:"),
+                # dcc.Dropdown(
+                #     id='country-id', 
+                #     value='Turkey', 
+                #     options=[{'value': x, 'label': x} 
+                #             for x in country],
+                #     clearable=False
+                # ),
+                dcc.Graph(id ='table-1', config={
         "displaylogo": False
     },
                     ),
@@ -321,7 +386,111 @@ def generate_cloud(words):
         #fig.show()
     return fig
 
+@app.callback(
+    Output("table-1", "figure"),
+    [Input("words-1", "value"), Input("word-id", "value")])
+def generate_cloud(words_1, word_id):
+    fig = go.Figure()
+    if words_1 == "POSITIVE":
+        Englishnlp1 = Englishnlp[(Englishnlp.sentimentAnalysis != 'NEGATIVE') & (Englishnlp.sentimentAnalysis != 'undetermined')]
+        Englishnlp2 = Englishnlp1[(Englishnlp1.enProcessed == word_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Englishnlp2['enProcessed'], # 1st column
+                       Englishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])       
+        #fig.show()
+    elif words_1 == "NEGATIVE":
+        Englishnlp1 = Englishnlp[(Englishnlp.sentimentAnalysis != 'POSITIVE') & (Englishnlp.sentimentAnalysis != 'undetermined')]
+        Englishnlp2 = Englishnlp1[(Englishnlp1.enProcessed == word_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Englishnlp2['enProcessed'], # 1st column
+                       Englishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])       
 
+        #fig.show()
+    elif words_1 == "undetermined":
+        Englishnlp1 = Englishnlp[(Englishnlp.sentimentAnalysis != 'POSITIVE') & (Englishnlp.sentimentAnalysis != 'NEGATIVE')]
+        Englishnlp2 = Englishnlp1[(Englishnlp1.enProcessed == word_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Englishnlp2['enProcessed'], # 1st column
+                       Englishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+]) 
+        #fig.show()
+    return fig
+
+@app.callback(
+    Output("table-2", "figure"),
+    [Input("words2-1", "value"), Input("word2-id", "value")])
+def generate_cloud(words2_1, word2_id):
+    fig = go.Figure()
+    if words2_1 == "positive":
+        Turkishnlp1 = Turkishnlp[(Turkishnlp.sentimentAnalysis != 'negative') & (Turkishnlp.sentimentAnalysis != 'undetermined')]
+        Turkishnlp2 = Turkishnlp1[(Turkishnlp1.trProcessed == word2_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Turkishnlp2['trProcessed'], # 1st column
+                       Turkishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])       
+        #fig.show()
+    elif words2_1 == "negative":
+        Turkishnlp1 = Turkishnlp[(Turkishnlp.sentimentAnalysis != 'positive') & (Turkishnlp.sentimentAnalysis != 'undetermined')]
+        Turkishnlp2 = Turkishnlp1[(Turkishnlp1.trProcessed == word2_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Turkishnlp2['trProcessed'], # 1st column
+                       Turkishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])       
+
+        #fig.show()
+    elif words2_1 == "undetermined":
+        Turkishnlp1 = Turkishnlp[(Turkishnlp.sentimentAnalysis != 'positive') & (Turkishnlp.sentimentAnalysis != 'negative')]
+        Turkishnlp2 = Turkishnlp1[(Turkishnlp1.enProcessed == word2_id)]
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=['Word Selected', 'Response'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+        cells=dict(values=[Turkishnlp2['trProcessed'], # 1st column
+                       Turkishnlp2['10']], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+]) 
+        #fig.show()
+    return fig
 # # Bar Chart Callback
 
 
@@ -357,4 +526,4 @@ def generate_cloud(words):
 #     app.run_server(debug=True)
 
 def layout4():
-    return html.Div(children=[Layout])
+    return html.Div(children=[BODY])
